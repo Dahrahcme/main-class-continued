1e17891ca0eb9c358da4111bae7a68608693f419
Dahrah <tripleo4real@gmail.com>
2023-05-16 14:17:03 +0100

    work for 16-05-23
    revision

---
+++ 16-05-23/revision/index.js
@@ -0,0 +1,40 @@
+// Concatenation
+
+var orukoMi = "Hello"
+
+var orukoWa = " World!"
+
+var oruko = orukoMi + orukoWa
+
+console.log(oruko);
+
+// Class Work
+
+let num1 = window.prompt("Enter first number: ")
+num11 = parseInt(num1, 10)
+
+let num2 = window.prompt("Enter second number: ")
+num21 = Number(num2)
+
+let num3 = window.prompt("Enter third number: ")
+num31 = Number(num3)
+
+let num4 = window.prompt("Enter fourth number: ")
+num41 = Number(num4)
+
+let num5 = window.prompt("Enter fifth number: ")
+num51 = Number(num5)
+
+let average = window.alert((num11 + num21 + num31 + num41 + num51) / 5)
+
+// console.log(average);
+
+// Functions
+
+function add() {
+    console.log(a + b);
+}
+
+// Calling a function
+
+add()
